2c01c72b243253297c15cfcc5ff3d7d1501ad264
diego0172 <dchamale@gtc.com.gt>
2025-08-01 17:15:54 -0600

    Initial commit

---
+++ instal.dependencias.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "📦 Instalando dependencias para el bot de WhatsApp..."
+
+npm install express
+npm install whatsapp-web.js
+npm install pg
+npm install qrcode
+npm install xlsx
+npm install dotenv
+npm install jspdf
+npm install jspdf-autotable
+npm install moment
+
+npm install @google-cloud/dialogflow                
+
+echo "✅ Todas las dependencias fueron instaladas correctamente."
